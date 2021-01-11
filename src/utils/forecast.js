8e2d39bacf2e2b19fbd1f634d10decf777c7cded
Wayner Moya <waynermoya@hotmail.com>
2021-01-11 17:48:15 -0500

    nuevos cambios

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,6 +10,8 @@ forecast=(addres , callback)=>{
         }else if(body.error){
             callback("Unable to find location" , undefined)
         }else{
+            
+
             callback(undefined , {
                 weather_descriptions : body.current.weather_descriptions[0],
                 temperature : body.current.temperature,
